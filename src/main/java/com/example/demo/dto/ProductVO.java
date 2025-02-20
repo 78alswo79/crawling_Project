0b7cff576dd420fe5472b2eae28d9726726f9d6e
DavidLee <78alswo79@gmail.com>
2025-02-20 23:50:59 +0900

    getter/setter 작업 추가

--- src/main/java/com/example/demo/dto/ProductVO.java
+++ src/main/java/com/example/demo/dto/ProductVO.java
@@ -5,38 +5,86 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ProductVO {
 
-	private String title;
-	private int price;
+	private String bookTitle;
+	private int bookprice;
+	private String imgSrc;
+	private String searchResult;
+	private String customStarRating;
+	private String detailHref;
 	
 	// 기본 생성자 추가 (선택 사항)
     public ProductVO() {
     }
-
-	public ProductVO(String title, int price) {
-		this.title = title;
-		this.price = price;
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
+    
+    public ProductVO(String bookTitle, int bookprice, String imgSrc, String searchResult, String customStarRating,
+			String detailHref) {
+		super();
+		this.bookTitle = bookTitle;
+		this.bookprice = bookprice;
+		this.imgSrc = imgSrc;
+		this.searchResult = searchResult;
+		this.customStarRating = customStarRating;
+		this.detailHref = detailHref;
 	}
 
-	public int getPrice() {
-		return price;
+	public String getBookTitle() {
+		return bookTitle;
 	}
 
-	public void setPrice(int price) {
-		this.price = price;
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+
+	public int getBookprice() {
+		return bookprice;
+	}
+
+	public void setBookprice(int bookprice) {
+		this.bookprice = bookprice;
+	}
+
+	public String getImgSrc() {
+		return imgSrc;
+	}
+
+	public void setImgSrc(String imgSrc) {
+		this.imgSrc = imgSrc;
+	}
+
+	public String getSearchResult() {
+		return searchResult;
+	}
+
+	public void setSearchResult(String searchResult) {
+		this.searchResult = searchResult;
+	}
+
+	public String getCustomStarRating() {
+		return customStarRating;
+	}
+
+	public void setCustomStarRating(String customStarRating) {
+		this.customStarRating = customStarRating;
+	}
+
+	public String getDetailHref() {
+		return detailHref;
+	}
+
+	public void setDetailHref(String detailHref) {
+		this.detailHref = detailHref;
 	}
 
 	@Override
 	public String toString() {
-		return "ProductVO [title=" + title + ", price=" + price + "]";
+		return "ProductVO [bookTitle=" + bookTitle + ", bookprice=" + bookprice + ", imgSrc=" + imgSrc
+				+ ", searchResult=" + searchResult + ", customStarRating=" + customStarRating + ", detailHref="
+				+ detailHref + "]";
 	}
+
+	
+
+    
 	
 	
 	
